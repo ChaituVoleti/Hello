03b7320be52f8c96df44da84c7b0725e1799cff6
ChaituVoleti <146985525+ChaituVoleti@users.noreply.github.com>
2024-02-26 12:42:43 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int num1 = 10;
+        int num2 = 5;
+        int sum = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+        int product = num1 * num2;
+        System.out.println("Product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
